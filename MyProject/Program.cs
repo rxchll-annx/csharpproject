07432c1377a625c53d9b25d7b24f87477df0c02a
Rochelle Anne Barizo <rochelleannebarizo00@gmail.com>
2025-10-30 08:32:06 +0000

    Task1 commit message for submission

---
+++ MyProject/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Task1
+{
+    static void Main(string [] args)
+    {
+        Console.WriteLine("Kamusta Mundo!");
+    }
+}
